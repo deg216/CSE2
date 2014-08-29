489cb2cb33d2e6514d913353255c9b708582de3c
deg216 <deg216@lehigh.edu>
2014-08-29 23:18:57 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+//Devika Gupta
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.javac
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+      
+//  print the statement wih box around it
+    System.out.println("-------------");
+    System.out.println("|  WELCOME  |");
+    System.out.println("-------------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-D--E--G--2--1--6-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("My name is Devika Gupta. I am taking CSE2 for fun.");
+    
+    //test/testtest
+  }
+}
